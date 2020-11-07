8f591ece4d979ac08596cb3f9fc7eff699919a0d
nourhen <nourheneebarhoumii@gmail.com>
2020-11-07 21:09:08 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const DataJson = require('./data.json')
+require('dotenv').config()
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
+app.get('/data',(req,res)=>{
+    res.json(DataJson)
+})
+const PORT = process.env.PORT 
+
+app.listen(PORT,()=>{
+    console.log(`server listen on port ${PORT} `)
+})
